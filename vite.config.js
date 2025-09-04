f067d79448604b451d8dd99a2229f9f6e5d7afe7
Stéphan Mestach <smestach@bluesquare.org>
2025-09-04 22:00:48 +0200

    try to fix asset loading once deployed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dep-xlsx-js/' // Add base path for GitHub Pages deployment
+  base: '/dep-xlsx-js/', // Add base path for GitHub Pages deployment
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
